1c8e1ae4768f01b6b64f6ba9028ac6a2a7d308c8
Evox <evoxdl@gmail.com>
2026-06-01 01:03:28 +0500

    sm6450-common: Decommonize Kernel Modules

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,68 +91,6 @@ TARGET_KERNEL_CONFIG := \
 
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/motorola/sm6450-modules
 
-# Kernel Modules
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
-BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(TARGET_KERNEL_SOURCE)/modules.vendor_blocklist.msm.parrot
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot))
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
-BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
-
-TARGET_KERNEL_EXT_MODULES := \
-    qcom/opensource/mmrm-driver \
-    qcom/opensource/audio-kernel \
-    qcom/opensource/camera-kernel \
-    qcom/opensource/cvp-kernel \
-    qcom/opensource/dataipa/drivers/platform/msm \
-    qcom/opensource/datarmnet/core \
-    qcom/opensource/datarmnet-ext/aps \
-    qcom/opensource/datarmnet-ext/offload \
-    qcom/opensource/datarmnet-ext/shs \
-    qcom/opensource/datarmnet-ext/perf \
-    qcom/opensource/datarmnet-ext/perf_tether \
-    qcom/opensource/datarmnet-ext/sch \
-    qcom/opensource/datarmnet-ext/wlan \
-    qcom/opensource/display-drivers/msm \
-    qcom/opensource/eva-kernel \
-    qcom/opensource/video-driver \
-    qcom/opensource/wlan/qcacld-3.0/.adrastea \
-    qcom/opensource/wlan/qcacld-3.0/.qca6750
-
-TARGET_KERNEL_EXT_MODULES += \
-    motorola/drivers/mmi_annotate \
-    motorola/drivers/mmi_info \
-    motorola/drivers/backlight/aw99703 \
-    motorola/drivers/backlight/ktd3136 \
-    motorola/drivers/power/bm_adsp_ulog \
-    motorola/drivers/power/mmi_charger \
-    motorola/drivers/power/qti_glink_charger \
-    motorola/drivers/power/qpnp_adaptive_charge \
-    motorola/drivers/power/bq27426_fg_mmi \
-    motorola/drivers/power/sgm4154x_charger_lite \
-    motorola/drivers/misc/utag \
-    motorola/drivers/misc/mmi_stow \
-    motorola/drivers/mmi_relay \
-    motorola/drivers/misc/mmi_sys_temp \
-    motorola/drivers/regulator/dio8015 \
-    motorola/drivers/regulator/wl2864c \
-    motorola/drivers/regulator/wl2866d \
-    motorola/drivers/regulator/slg5bm43670 \
-    motorola/drivers/sensors \
-    motorola/drivers/misc/awinic/sarsensor \
-    motorola/drivers/misc/stk501xx \
-    motorola/drivers/misc/sx937x \
-    motorola/drivers/input/touchscreen/touchscreen_mmi \
-    motorola/drivers/input/touchscreen/focaltech_0flash_v2_mmi \
-    motorola/drivers/input/touchscreen/ili9882_mmi \
-    motorola/drivers/input/touchscreen/goodix_berlin_mmi \
-    motorola/drivers/input/misc/anc_fps_mmi \
-    motorola/drivers/input/misc/fpc_fps_mmi \
-    motorola/drivers/input/misc/goodix_fod_mmi \
-    motorola/drivers/moto_netopt/con_dfpar \
-    motorola/drivers/nfc/st21nfc \
-    motorola/drivers/wlan_antenna
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
